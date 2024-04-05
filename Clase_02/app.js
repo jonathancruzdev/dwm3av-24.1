78989831dfbdeb6429d360add8dbdc790723cad6
Jonathan Cruz <>
2024-04-05 15:05:26 -0300

    Clase 02 - Directivas I

---
+++ Clase_02/app.js
@@ -0,0 +1,15 @@
+console.log('Clase 02');
+const app = Vue.createApp({
+    // El Model
+    data(){
+        return {
+            titulo: 'Instancia de Vue',
+            mensaje: 'Bienvenido a VueJS',
+            foto: 'images/gatito.png',
+            mascota: 'Gatito',
+            logueado: true
+        }
+    }
+});
+
+app.mount('#contenedorApp');
