58dcb17a4f397eac5f914fe2b277707e8cbb3f67
Jonathan Cruz <>
2024-04-12 15:35:39 -0300

    Clase 03 - Directivas

--- Clase_03/app.js
+++ Clase_03/app.js
@@ -7,17 +7,27 @@ const app = Vue.createApp({
             tareas: [
                 {fecha: '12/04/24', descripcion: 'Leer un libro', completado: false },
                 {fecha: '10/04/24', descripcion: 'Pasear al perro', completado: true }
-
             ]
         }
+    },
+    created(){
+        console.log('Created');
+        const tareas = JSON.parse( localStorage.getItem('task'));
+        this. tareas = tareas ? tareas : [];
     },
     // Métodos
     methods: {
         eliminar(index){
             console.log('Eliminando tarea ' , index);
             this.tareas.splice(index, 1);
+            this.guardar();
         },
-        guardar(){
+        completar(task){
+            console.log('Completando tarea', task);
+            task.completado = true;
+            this.guardar();
+        },
+        agregar(){
             if( this.tarea.trim() == ''){
                 alert('Completar la descripción de la tarea');
                 return;
@@ -30,6 +40,10 @@ const app = Vue.createApp({
                 completado: false
             });
             this.tarea = '';
+            this.guardar();
+        },
+        guardar(){
+            localStorage.setItem('task', JSON.stringify( this.tareas));
         }
     }
 })
