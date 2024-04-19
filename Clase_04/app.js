d1df29cd57d12776f366d2eeca86551300b0f6e2
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2024-04-19 15:16:10 -0300

    Clase 04 - Componentes

--- Clase_04/app.js
+++ Clase_04/app.js
@@ -8,7 +8,6 @@ const app = Vue.createApp({
                 {id:1 , name: 'Gatito 1', img: 'images/gatito1.jpg', age: 1},
                 {id:2 , name: 'Gatito 2', img: 'images/gatito2.jpg', age: 2},
                 {id:3 , name: 'Gatito 3', img: 'images/gatito3.jpg', age: 1},
-
             ]
         }
     },
@@ -24,11 +23,7 @@ const app = Vue.createApp({
 
 /* ------------------------------- Componetes ------------------------------- */
 app.component('titulo', {
-    data(){
-        return {
-            texto : ' Hoy soy un título'
-        }
-    },
+    props: ['texto'],
     template: '<h2 class="text-info"> {{ texto}} </h2>'
 });
 
@@ -49,7 +44,7 @@ app.component('gatito', {
                 <div class="card"> 
                     <img v-bind:src="foto" class="card-img-top" alt="gatito">
                     <div class="card-body">
-                        <h4> {{ nombre }} </h4>
+                        <titulo v-bind:texto="nombre"></titulo>
                         <strong>Edad: </strong> <span> {{ edad }} </span>
                         <hr>
                         <button v-on:click="darLike" class="btn btn" type="button">❤ {{count}}</button>
