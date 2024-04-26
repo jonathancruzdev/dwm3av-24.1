9eb3cf70e8b0ac31000370780bf90f2e50f7a448
Jonathan Cruz <>
2024-04-26 15:34:50 -0300

    Clase 05 - localStorage & fetch

---
+++ Clase_05/app.js
@@ -0,0 +1,62 @@
+const app = Vue.createApp( {
+    data(){
+        return {
+            nombre: '',
+            apellido: '',
+            email: '',
+            cursoId: 3,
+            cursos: [],
+            inscripciones: []
+        }
+    },
+    created(){
+        this.getJSON();
+        this.leerLocal();
+    },
+    methods: {
+        async getJSON(){
+            try {
+                const resp = await fetch('api/data.json');
+                const json = await resp.json();
+                this.cursos = json.data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        obtenerCurso(id){
+            console.log(id)
+            const cursoNombre = this.cursos.find( item => item.id == id );
+            return cursoNombre ? cursoNombre.name : ''
+        },
+        inscribirse(){
+            const hora = new Date().toLocaleTimeString();
+            const fecha = new Date().toLocaleDateString();
+            const inscripcion = {
+                nombre: this.nombre,
+                apellido: this.apellido,
+                email: this.email,
+                cursoId: this.cursoId,
+                hora: hora,
+                fecha: fecha
+            }
+
+            this.inscripciones.push(inscripcion);
+            console.log(inscripcion, this.inscripciones);
+            this.guardarLocal();
+            // Limpio el formulario
+            this.nombre = '';
+            this.apellido = '';
+            this.email = '';
+            this.cursoId = '';
+        },
+        leerLocal(){
+            const storage = JSON.parse( localStorage.getItem('data') );
+            this.inscripciones = storage ? storage : [];
+        },
+        guardarLocal(){
+            localStorage.setItem('data', JSON.stringify(this.inscripciones));
+        }
+    }
+})
+
+app.mount('#app');
